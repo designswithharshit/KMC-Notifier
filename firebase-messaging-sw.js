89d6092752a6413f00a81b1e0211a64ede6be3dc
Harshit Kashyap <h.kking1415@gmail.com>
2026-03-05 21:15:50 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -29,6 +29,19 @@ messaging.onBackgroundMessage(function(payload) {
 self.addEventListener('notificationclick', function(event) {
   event.notification.close();
   event.waitUntil(
-    clients.openWindow(event.notification.data.url)
+    clients.matchAll({ type: "window", includeUncontrolled: true })
+    .then((clientList) => {
+    
+    for (const client of clientList) {
+        if (client.url === event.notification.data.url && "focus" in client) {
+            return client.focus();
+        }
+    }
+    
+    if (clients.openWindow) {
+        return clients.openWindow(event.notification.data.url);
+    }
+    });
   );
 });
+
